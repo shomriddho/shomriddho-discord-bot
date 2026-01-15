3edd38ca6c0d5c146d0be00dab72db362e34d4a8
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2026-01-16 00:56:12 +0600

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import { env } from "@shomriddho-discord-bot/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
